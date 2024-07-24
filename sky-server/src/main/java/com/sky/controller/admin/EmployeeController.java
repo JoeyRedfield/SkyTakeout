4c78ea72a8cb26ba34049a8d1610a27f82b2bf93
Zhuoyi Wu <1300163280@qq.com>
2024-07-24 20:26:49 +0800

    Day2 新增员工 业务代码

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,7 +1,9 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
 import com.sky.result.Result;
@@ -13,10 +15,7 @@ import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -65,6 +64,23 @@ public class EmployeeController {
 
         return Result.success(employeeLoginVO);
     }
+
+    @PostMapping
+    @ApiOperation("新增员工")
+    public Result<String> save (@RequestBody EmployeeDTO employeeDTO){
+
+//        System.out.println("线程id: " + Thread.currentThread().getId());
+
+        log.info("新增员工：{}", employeeDTO);
+        employeeService.save(employeeDTO);
+        return Result.success("创建成功");
+    }
+
+//    @ApiOperation("员工分页查询")
+//    @GetMapping("/page")
+//    public Result<> page(@RequestBody EmployeePageQueryDTO employeePageQueryDTO){
+//
+//    }
 
     /**
      * 退出
