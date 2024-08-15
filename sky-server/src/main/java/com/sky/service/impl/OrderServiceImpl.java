c19bb720a83e7d05c2683c806e78c095dbc8ddc4
Zhuoyi Wu <1300163280@qq.com>
2024-08-15 10:46:15 +0800

    Day10 客户催单

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -405,6 +405,27 @@ public class OrderServiceImpl implements OrderService {
         orderMapper.update(orders);
     }
 
+    @Override
+    public void reminder(Long id) {
+        // 根据id查询订单
+        Orders ordersDB = orderMapper.getById(id);
+
+        // 校验订单是否存在
+        if (ordersDB == null) {
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+
+        // 通过websocket向客户端浏览器推送信息 type orderId content
+        Map map = new HashMap();
+        map.put("type", 2); // 1表示来单提醒, 2表示客户催单
+        map.put("orderId",ordersDB.getId());
+        map.put("content","订单号"+ordersDB.getNumber());
+
+        String json = JSON.toJSONString(map);
+        webSocketServer.sendToAllClient(json);
+
+    }
+
     /**
      * 支付成功，修改订单状态
      *
